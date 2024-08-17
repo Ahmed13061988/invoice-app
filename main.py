9358823ddb6abb90e7b4f70d319fb4f738c597b8
Ahmed Hussein <wolktheline@yahoo.com>
2024-08-17 13:47:42 -0600

    reading the content of excel files.

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+from fpdf import FPDF
+import pandas as pd
+import glob
+
+files_path = glob.glob("invoices/*.xlsx")
+
+for i in files_path:
+    df = pd.read_excel(i, sheet_name="Sheet 1")
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
+    pdf.add_page()
 
