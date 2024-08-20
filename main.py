1abbe28a81c4290cd72470293b6a0de3bfa40ae1
Ahmed Hussein <wolktheline@yahoo.com>
2024-08-19 23:28:27 -0600

    Adding a table with headers and the data.

--- main.py
+++ main.py
@@ -18,7 +18,23 @@ for i in files_path:
     pdf.cell(w=50, h=8, txt=f"Invoice No.{invoice_no}", ln=1)
 
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"Date: {date}")
+    pdf.cell(w=50, h=8, txt=f"Date: {date}", ln=1)
+
+    columns = list(df.columns)
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.cell(w=30, h=8, txt=columns[0].replace("_", " ").title(), border=1)
+    pdf.cell(w=70, h=8, txt=columns[1].replace("_", " ").title(), border=1)
+    pdf.cell(w=33, h=8, txt=columns[2].replace("_", " ").title(), border=1)
+    pdf.cell(w=30, h=8, txt=columns[3].replace("_", " ").title(), border=1)
+    pdf.cell(w=30, h=8, txt=columns[4].replace("_", " ").title(), border=1, ln=1)
+
+    for index, row in df.iterrows():
+        pdf.set_font(family="Times", size=10)
+        pdf.set_text_color(80, 80, 80)
+        pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
+        pdf.cell(w=70, h=8, txt=str(row["product_name"]), border=1)
+        pdf.cell(w=33, h=8, txt=str(row["amount_purchased"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
 
     pdf.output(f"results/{filename}.pdf")
-
